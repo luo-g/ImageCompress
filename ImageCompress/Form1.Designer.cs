2bfc8a6cfc3bd5b0cddc2253d09f7ae801b980b9
luo-g <luo_guang@126.com>
2023-05-22 15:44:25 +0800

    添加项目文件。

---
+++ ImageCompress/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace ImageCompress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbThreadCount = new System.Windows.Forms.Label();
+            this.txtThreadCount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.Location = new System.Drawing.Point(420, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件夹";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "图片文件夹：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(501, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始压缩";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(14, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "是否替换原始图片";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 238);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(574, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(606, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(138, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(347, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "如未选择“替换原始图片”压缩图片将存放在“.temp”文件夹下";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "70";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "压缩大小：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "%";
+            // 
+            // lbThreadCount
+            // 
+            this.lbThreadCount.AutoSize = true;
+            this.lbThreadCount.Location = new System.Drawing.Point(12, 172);
+            this.lbThreadCount.Name = "lbThreadCount";
+            this.lbThreadCount.Size = new System.Drawing.Size(53, 12);
+            this.lbThreadCount.TabIndex = 11;
+            this.lbThreadCount.Text = "进程数：";
+            // 
+            // txtThreadCount
+            // 
+            this.txtThreadCount.Location = new System.Drawing.Point(107, 169);
+            this.txtThreadCount.Name = "txtThreadCount";
+            this.txtThreadCount.Size = new System.Drawing.Size(100, 21);
+            this.txtThreadCount.TabIndex = 12;
+            this.txtThreadCount.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(215, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "将图片按数量等分给进程处理";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 288);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtThreadCount);
+            this.Controls.Add(this.lbThreadCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "图片压缩工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbThreadCount;
+        private System.Windows.Forms.TextBox txtThreadCount;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ImageCompress/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ImageCompress
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 打开文件夹（多选）组件
+        /// </summary>
+        private CommonOpenFileDialog folderBrowserDialog1;
+        private IEnumerable<string> userSelectFolderPaths;
+        public Form1()
+        {
+            InitializeComponent();
+            folderBrowserDialog1 = new CommonOpenFileDialog();
+            //设置为选择文件夹
+            this.folderBrowserDialog1.IsFolderPicker = true;
+            //设置为多选
+            this.folderBrowserDialog1.Multiselect = true;
+            //设置标题
+            this.folderBrowserDialog1.Title = "选择要处理的文件夹(支持多选)";
+            //设置
+            this.folderBrowserDialog1.RestoreDirectory = true;
+            this.textBox1.Text = AppDomain.CurrentDomain.BaseDirectory;
+            this.folderBrowserDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //获得用户选择文件夹(多个)
+                userSelectFolderPaths = this.folderBrowserDialog1.FileNames;
+                this.textBox1.Text = string.Join(";", userSelectFolderPaths);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.progressBar1.Maximum = 0;
+            CheckForIllegalCrossThreadCalls = false;
+            this.button2.Enabled = false;
+            //创建临时文件夹
+            var tempPath = AppDomain.CurrentDomain.BaseDirectory + "\\.temp\\";
+            if (!Directory.Exists(tempPath))
+            {
+                Directory.CreateDirectory(tempPath);
+            }
+            this.label2.Text = "0/" + this.progressBar1.Maximum;
+            this.progressBar1.Value = 0;
+                List<KeyValuePair<string,string>> files = new List<KeyValuePair<string, string>>();
+            foreach (var currentPath in userSelectFolderPaths)
+            {
+                var soursePath = currentPath + "\\";
+                var currentPaths = currentPath.Split('\\');
+                var dir = currentPaths[currentPaths.Length - 1];
+                if (!Directory.Exists(tempPath + dir))
+                {
+                    Directory.CreateDirectory(tempPath + dir);
+                }
+                //读取图片
+                foreach (var f in Directory.GetFiles(soursePath, ".", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".png") || s.EndsWith(".jpg") || s.EndsWith(".gif")))
+                {
+                    this.progressBar1.Maximum += 1;
+                    var dic = new KeyValuePair<string, string>(dir, f);
+                    files.Add(dic);
+                }
+               
+            }
+            int percent;
+            int threadCount;
+            int index = 0;
+            if(!Int32.TryParse(this.txtThreadCount.Text,out threadCount))
+            {
+                threadCount = 1;
+            }
+            for(var i=0;i< threadCount;i++)
+            {
+                var fileCount = files.Count;
+                var count = fileCount / threadCount;
+                if(i==0&& fileCount%threadCount>0)
+                {
+                    count = count + (fileCount % threadCount);
+                }
+                if (Int32.TryParse(this.textBox2.Text, out percent))
+                {
+                    if (percent < 100 && percent > 0)
+                    {
+                        new System.Threading.Thread(() =>
+                        {
+                            new Action(() =>
+                            {
+                                for(var j= index; j< count;j++)
+                                {
+                                    var fileDic = files[j];
+                                    var file = fileDic.Value;
+                                    var dir = fileDic.Key;
+                                    var filePaths = file.Split('\\');
+                                    var tempFile = tempPath + dir + "\\" + filePaths[filePaths.Length - 1];
+                                    ImageHelper.Compress(file, tempFile, 100, percent);
+                                    if (this.checkBox1.Checked)
+                                    {
+                                        //替换原始文件
+                                        new FileInfo(tempFile).CopyTo(file, true);
+                                        File.Delete(tempFile);
+                                    }
+                                    this.progressBar1.Value += 1;
+                                    this.label2.Text = this.progressBar1.Value + "/" + this.progressBar1.Maximum;
+                                }
+                                if (this.progressBar1.Value == this.progressBar1.Maximum)
+                                    this.button2.Enabled = true;
+                            }).Invoke();
+                        }).Start();
+                        index += count;
+
+                    }
+
+                }
+
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
